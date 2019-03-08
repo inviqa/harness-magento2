c384b065fb4418b4a3a8e524534db059c094ac33
David Cole <dcole@inviqa.com>
2019-03-08 19:10:07 +0000

    pull and up on install and start

--- harness/scripts/enable.sh
+++ harness/scripts/enable.sh
@@ -1,39 +1,29 @@
 #!/usr/bin/env bash
 
 if [ ! -f .flag-built ]; then
-
     passthru docker-compose -p "$NAMESPACE" down
 
     [[ "$HAS_ASSETS" = "yes" ]] && ws assets download
 
-    if [ "$APP_BUILD" = "dynamic" ] && [ "${USE_DOCKER_SYNC}" = "yes" ]; then
+    if [[ "$APP_BUILD" = "dynamic" && "$USE_DOCKER_SYNC" = "yes" ]]; then
         passthru docker-sync start
         passthru docker-sync stop
     fi
 
-    if [ "$APP_BUILD" = "static" ]; then
-        passthru docker-compose -p "$NAMESPACE" build --no-cache console
-        passthru docker-compose -p "$NAMESPACE" up -d console
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
-        [[ "${APP_MODE}" = "production" ]] && passthru docker-compose -p "$NAMESPACE" exec -T -u build console app build pass-2
-        passthru docker commit "${NAMESPACE}_console_1" "${NAMESPACE}_console:latest"
-        passthru docker-compose -p "$NAMESPACE" build --no-cache nginx php-fpm
-        passthru docker-compose -p "$NAMESPACE" up -d
-    else
-        passthru docker-compose -p "$NAMESPACE" up -d --build
+    passthru docker-compose -p "$NAMESPACE" pull
+    passthru docker-compose -p "$NAMESPACE" up -d --build
+
+    if [[ "$APP_BUILD" = "dynamic" ]]; then
         passthru docker-compose -p "$NAMESPACE" exec -T -u build console app build
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
     fi
-
+    
+    passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
     touch .flag-built
-
 else
-    run docker-compose -p "$NAMESPACE" start
+    run docker-compose -p "$NAMESPACE" up -d
     passthru docker-compose -p "$NAMESPACE" exec -T -u build console app welcome
 fi
 
-if [ "$APP_BUILD" = "dynamic" ]; then
-    if [[ "$USE_DOCKER_SYNC" = "yes" ]]; then
-        passthru docker-sync start
-    fi
+if [[ "$APP_BUILD" = "dynamic" && "$USE_DOCKER_SYNC" = "yes" ]]; then
+    passthru docker-sync start
 fi

cb3018ef19f132d3ace2cb0cc1a384c663802e9e
test <dcole@inviqa.com>
2019-05-23 12:45:14 +0100

    publish 'magento2' harness

---
+++ docker/image/console/root/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+main()
+{
+    su - build
+    sh -c 'while sleep 3600; do :; done'
+}
+
+setup_app_networking()
+{
+    # make linux consistent with docker-for-mac
+    if [ "${HOST_OS_FAMILY}" = "linux" ]; then
+        DOCKER_INTERNAL_HOST="host.docker.internal"
+        if ! grep $DOCKER_INTERNAL_HOST /etc/hosts > /dev/null ; then
+            DOCKER_INTERNAL_IP=$(/sbin/ip route|awk '/default/ { print $3 }')
+            echo -e "$DOCKER_INTERNAL_IP    $DOCKER_INTERNAL_HOST" | tee -a /etc/hosts > /dev/null
+        fi
+    fi
+}
+
+bootstrap()
+{
+    setup_app_networking
+}
+
+bootstrap
+main
